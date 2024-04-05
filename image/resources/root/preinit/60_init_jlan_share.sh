baac7b0c8b69cb9b94dc5ed2d16212964a822d46
dSeidel <d.seidel@alkacon.com>
2024-04-05 09:40:59 +0200

    Removed JLAN option (WebDav is preferred).

--- image/resources/root/preinit/60_init_jlan_share.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [ "${ENABLE_JLAN}" == "true" ]; then
-	echo "Enabling default Jlan SMB Share for OpenCms"
-	mv -v ${OPENCMS_HOME}/WEB-INF/config/jlanConfig.xml.linux ${OPENCMS_HOME}/WEB-INF/config/jlanConfig.xml
-else
-	echo "Jlan SMB Share remains disabled. Start the container with \"-e ENABLE_JLAN=true\" to enable it."
-fi
